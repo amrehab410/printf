e204ca8d97e983c4e6487097d9b0ee61fc5fb064
root <root@DESKTOP-LP5R8B0.localdomain>
2023-11-12 22:17:39 +0200

    file_sepration

--- formatloop.c
+++ formatloop.c
@@ -1,30 +1,11 @@
 #include "main.h"
 /**
  * formatloop - specifies which format specifer we are using
- * @spec: the format specifier.
- * @str: the argument used.
+ * @format: the format string.
+ * @list: the list of args used.
  * Return: the value of the argument
  */
-/**
- * char *formatloop(char spec, char *str)
- * {
- * 	switch (spec)
- * 	{
- * 	case 'c':
- * 		return (char_check(str));
- *
- * 	 *case 's':
- * 	 *return (string_check(str));
- * 	 *case 'd':
- * 	 *return (dec_check(str));
- * 	 *case 'i':
- * 	 *return (int_check(str));
- *
- * 	default:
- * 		return (NULL);
- * 	}
- * }
- */
+
 int formatloop(const char *format, va_list list)
 {
 	unsigned long int i, len;
--- printf.c
+++ printf.c
@@ -4,51 +4,6 @@
  * @format: the string
  * Return: Always 0
  */
-/**
-* int _printf(const char *format, ...)
-* {
-* 	unsigned long int i, len, count = 0;
-* 	char *str, percent = '%';
-* 	int c;
-* 	va_list list;
-
-* 	va_start(list, format);
-* 	for (i = 0; i < strlen(format); i++)
-* 	{
-* 		if (format[i] == '%')
-* 		{
-* 			if (format[i + 1] == '%')
-* 			{
-* 				write(1, &percent, 1);
-* 				i++;
-* 				count++;
-* 			}
-* 			else if (format[i + 1] == 'c')
-* 			{
-* 				c = va_arg(list, int);
-* 				write(1, &c, 1);
-* 				i++;
-* 				count++;
-* 			}
-* 			else if (format[i + 1] == 's')
-* 			{
-* 				str = va_arg(list, char *);
-* 				len = strlen(str);
-* 				write(1, str, len);
-* 				i++;
-* 				count += len;
-* 			}
-* 		}
-* 		else
-* 		{
-* 			write(1, &format[i], 1);
-* 			count++;
-* 		}
-* 	}
-* 	va_end(list);
-* 	return (count);
-* }
-*/
 
 int _printf(const char *format, ...)
 {
