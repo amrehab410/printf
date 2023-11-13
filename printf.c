538d3ef2762d815a2bed79639bdfa9e40346792c
root <root@DESKTOP-1N3TEFC>
2023-11-13 17:28:07 +0200

    handle null

--- printf.c
+++ printf.c
@@ -10,6 +10,8 @@ int _printf(const char *format, ...)
 	va_list list;
 	int count;
 
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
 	va_start(list, format);
 	count = formatloop(format, list);
 	va_end(list);
